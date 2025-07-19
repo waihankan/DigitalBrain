18e547e9d4cae4c9da68b49891fab1c5ac86ef25
Wai Han <42233059+waihankan@users.noreply.github.com>
2025-07-19 11:16:09 -0700

    Quartz sync: Jul 19, 2025, 11:16 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Digital Brain",
+    pageTitle: "dev/brain",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
